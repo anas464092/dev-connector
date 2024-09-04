221afdb9fac1f10a298780e63dcc44bf50a4f5ef
anas464092 <anasrehman464092@gmail.com>
2024-09-04 18:42:41 +0500

    adding protected route funtionality

--- frontend/src/components/Protected.jsx
+++ frontend/src/components/Protected.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Login from '../Pages/Login';
+import { useSelector } from 'react-redux';
 
 function Protected() {
-    const loggedin = true;
-    return loggedin ? <Outlet /> : <Login />;
+    const { userInfo } = useSelector((state) => state.auth);
+    return userInfo ? <Outlet /> : <Login />;
 }
 
 export default Protected;
